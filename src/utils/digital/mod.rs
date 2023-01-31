4d9767796bfc485f14121404d0c199fbb3657e46
JM <j.maxwell.alan@gmail.com>
2023-01-31 16:03:20 -0500

    impl digital for u64

--- src/utils/digital/mod.rs
+++ src/utils/digital/mod.rs
@@ -12,3 +12,22 @@ pub use self::{
 pub trait Digital {
   fn as_digits(&self) -> Vec<u8>;
 }
+
+impl Digital for u64 {
+  #[inline(always)]
+  fn as_digits(&self) -> Vec<u8> {
+    if self < &10 {
+      return vec![*self as u8];
+    }
+    let mut copied = *self;
+
+    let mut result = vec![];
+
+    while copied > 0 {
+      result.push((copied % 10) as u8);
+      copied /= 10
+    }
+
+    result
+  }
+}
