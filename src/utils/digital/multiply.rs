aad4ad49285c1021c1b607098fa85e3b650de21e
JM <j.maxwell.alan@gmail.com>
2023-02-12 04:18:12 -0500

    complete scalar multiplication algorithm

---
+++ src/utils/digital/multiply.rs
@@ -0,0 +1,29 @@
+#[inline(always)]
+pub fn digital_scalar_multiply_in_place_u32(lhs: &mut [u32], rhs: u32) -> u32 {
+  let mut carry = 0;
+
+  for el in lhs.iter_mut() {
+    let after_fit = fit_shift(higher_order_multiply(*el, rhs), el);
+    *el += carry;
+    carry = after_fit;
+  }
+
+  carry
+}
+
+#[inline(always)]
+fn higher_order_multiply(lhs: u32, rhs: u32) -> u64 {
+  lhs as u64 * rhs as u64
+}
+
+#[inline(always)]
+fn fit_shift(val: u64, target: &mut u32) -> u32 {
+  let ceil_val = u32::MAX as u64;
+  if val <= ceil_val {
+    *target = val as u32;
+    0
+  } else {
+    *target = val as u32;
+    (val >> 32) as u32
+  }
+}
