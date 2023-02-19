897d08ddd4fd02a9198bbc4b2c08bc0ec4292b16
JM <j.maxwell.alan@gmail.com>
2023-02-19 12:58:20 -0500

    correct major flaw in digital-digital multiplication

--- src/utils/digital/multiply.rs
+++ src/utils/digital/multiply.rs
@@ -1,6 +1,45 @@
 use std::ops::Mul;
 
-use super::{wrapping_add, DigitalWrap, Sign};
+use super::{digital_add, wrapping_add, DigitalWrap, Sign};
+use crate::{bigint::BigInt, utils::digital_add_in_place};
+
+#[inline(always)]
+pub fn digital_multiply_u32(lhs: &[u32], rhs: &[u32]) -> Vec<u32> {
+  match (lhs.len(), rhs.len()) {
+    (0, _) => rhs.to_owned(),
+    (_, 0) => lhs.to_owned(),
+    (1, 1) => {
+      let high_res = higher_order_multiply(lhs[0], rhs[0]);
+      if high_res > u32::MAX as u64 {
+        vec![high_res as u32, (high_res >> 32) as u32]
+      } else {
+        vec![high_res as u32]
+      }
+    }
+    (1, _) => digital_scalar_multiply_u32(rhs, lhs[0]),
+    (_, 1) => digital_scalar_multiply_u32(lhs, rhs[0]),
+    (left_magnitude, right_magnitude) => {
+      let result_magnitude = left_magnitude + right_magnitude;
+
+      let standing_zeroes = vec![0; result_magnitude];
+      let mut scaled_mult = vec![0; result_magnitude];
+      let mut result = vec![0; result_magnitude];
+
+      for (idx, digit) in rhs.iter().enumerate() {
+        scaled_mult.copy_from_slice(&standing_zeroes);
+
+        scaled_mult[idx..(idx + left_magnitude)]
+          .copy_from_slice(&lhs[0..left_magnitude]);
+
+        digital_scalar_multiply_in_place_u32(&mut scaled_mult, *digit);
+
+        digital_add_in_place(&mut result, &scaled_mult, DigitalWrap::Max);
+      }
+
+      result
+    }
+  }
+}
 
 #[inline(always)]
 pub fn digital_scalar_multiply_in_place_u32(lhs: &mut Vec<u32>, rhs: u32) {
