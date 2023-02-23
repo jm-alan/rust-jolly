e2e512b5b520d5103656640540bac8a4ce89e2c2
JM <j.maxwell.alan@gmail.com>
2023-02-23 15:27:54 -0500

    create fixed width karatsuba multiplication

--- src/utils/digital/multiply/fixed_width.rs
+++ src/utils/digital/multiply/fixed_width.rs
@@ -1,6 +1,7 @@
 use super::{fit_shift, higher_order_multiply};
 use crate::utils::{
-  digital::add::digital_add_in_place_fixed_width, wrapping_add, DigitalWrap,
+  digital::add::digital_add_in_place_fixed_width, digital_add_fixed_width,
+  digital_subtract_fixed_width, wrapping_add, DigitalWrap,
 };
 
 #[inline(always)]
@@ -90,5 +91,111 @@ pub fn digital_scalar_multiply_in_place_u32_fixed_width(
     *el = wrapping_add(*el, carry, DigitalWrap::Max);
 
     carry = after_fit + (*el < before_carry) as u32;
+  }
+}
+
+#[inline(always)]
+pub fn karatsuba_mul_fixed_width(
+  lhs: &[u32],
+  rhs: &[u32],
+  crossover: usize,
+) -> [u32; 65536] {
+  match (lhs.len(), rhs.len()) {
+    (0, _) | (_, 0) | (1, _) | (_, 1) => {
+      digital_multiply_u32_into_fixed_width(lhs, rhs)
+    }
+    (left_magnitude, right_magnitude) => {
+      let should_k_recursive =
+        left_magnitude >= crossover && right_magnitude >= crossover;
+
+      let mut result = [0; 65536];
+
+      let larger_magnitude = usize::max(left_magnitude, right_magnitude);
+      let half = larger_magnitude >> 1;
+
+      let a: Vec<u32> = lhs
+        .iter()
+        .copied()
+        .skip(half)
+        .take(lhs.len().wrapping_sub(half))
+        .collect();
+      let b: Vec<u32> = lhs.iter().copied().take(half).collect();
+      let c: Vec<u32> = rhs
+        .iter()
+        .copied()
+        .skip(half)
+        .take(rhs.len().wrapping_sub(half))
+        .collect();
+      let d: Vec<u32> = rhs.iter().copied().take(half).collect();
+
+      let digital_ac = if should_k_recursive {
+        karatsuba_mul_fixed_width(&a, &c, crossover)
+      } else {
+        digital_multiply_u32_into_fixed_width(&a, &c)
+      };
+
+      let digital_bd = if should_k_recursive {
+        karatsuba_mul_fixed_width(&b, &d, crossover)
+      } else {
+        digital_multiply_u32_into_fixed_width(&b, &d)
+      };
+
+      let ad_bc_final = digital_subtract_fixed_width(
+        &digital_subtract_fixed_width(
+          &if should_k_recursive {
+            karatsuba_mul_fixed_width(
+              &digital_add_fixed_width(&a, &b, DigitalWrap::Max),
+              &digital_add_fixed_width(&c, &d, DigitalWrap::Max),
+              crossover,
+            )
+          } else {
+            digital_multiply_u32_into_fixed_width(
+              &digital_add_fixed_width(&a, &b, DigitalWrap::Max),
+              &digital_add_fixed_width(&c, &d, DigitalWrap::Max),
+            )
+          },
+          &digital_ac,
+          DigitalWrap::Max,
+        )
+        .0,
+        &digital_bd,
+        DigitalWrap::Max,
+      )
+      .0;
+
+      let mut half_zeroes = [0; 65536];
+      let mut full_zeroes = [0; 65536];
+
+      let mut adbc_zero_stop = 65535;
+      while ad_bc_final[adbc_zero_stop] == 0 && adbc_zero_stop > 0 {
+        adbc_zero_stop -= 1;
+      }
+
+      let mut ac_zero_stop = 65535;
+      while digital_ac[ac_zero_stop] == 0 && ac_zero_stop > 0 {
+        ac_zero_stop -= 1;
+      }
+
+      half_zeroes[half..65536]
+        .copy_from_slice(&ad_bc_final[0..=adbc_zero_stop]);
+
+      full_zeroes[(half * 2)..65536]
+        .copy_from_slice(&digital_ac[0..=ac_zero_stop]);
+
+      result.copy_from_slice(&digital_bd);
+
+      digital_add_in_place_fixed_width(
+        &mut result,
+        &half_zeroes,
+        DigitalWrap::Max,
+      );
+      digital_add_in_place_fixed_width(
+        &mut result,
+        &full_zeroes,
+        DigitalWrap::Max,
+      );
+
+      result
+    }
   }
 }
